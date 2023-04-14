86b2757284739bc16b4664f3148dcd7472d968d3
Anh Chau <anhchau8922@gmail.com>
2023-04-15 00:02:07 +0100

    update

---
+++ todo-list/src/components/heading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './anh.css';
+
+export default function heading() {
+    return(
+        <div id='header'>
+            <h1>liSTa</h1>
+            <div></div>
+        </div>
+    )
+}
