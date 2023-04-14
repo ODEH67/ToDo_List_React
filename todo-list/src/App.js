e9592e3961798d8bf5286be8b668d3050f9fb705
Anh Chau <anhchau8922@gmail.com>
2023-04-14 19:58:58 +0100

    install calendar

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { RiMenuUnfoldFill } from "react-icons/ri";
+import {RiLogoutBoxRLine} from 'react-icons/ri';
+import {RiAccountPinCircleFill} from 'react-icons/ri';
+import {RiCalendarEventFill} from 'react-icons/ri';
+import {RiSettings5Fill} from 'react-icons/ri';
+import {MdLanguage} from 'react-icons/md';
+import {CgDarkMode} from 'react-icons/cg';
 import './App.css';
-import './components/anh.css'
+import './components/anh.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <section className="side-bar">
+        <div className="menu">
+          <RiMenuUnfoldFill />
+        </div>
+        <div className="task-list">
+        <div id="nav">
+          <a><RiAccountPinCircleFill /></a>
+          <a><RiCalendarEventFill /></a>
+          <a><MdLanguage /></a>
+          <a><CgDarkMode /></a>
+          <a><RiSettings5Fill /></a>
+
+            
+        </div>
+        </div>
+        <div className="logout">
+          <RiLogoutBoxRLine />
+        </div>
+      </section>
+      <section className="workspace">This is where main task display
+
+      </section>
+      <section className="calendar">calendar
+      </section>
     </div>
+
   );
 }
 
