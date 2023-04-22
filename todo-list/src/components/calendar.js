c7f15c0b95f784f34364819b606f15c89cf0356e
Anh Chau <anhchau8922@gmail.com>
2023-04-22 11:47:27 +0100

    update

--- todo-list/src/components/calendar.js
+++ todo-list/src/components/calendar.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 //import 'react-calendar/dist/Calendar.css';
 import './anh-calendar.css'
@@ -6,6 +6,12 @@ import './anh-calendar.css'
 export default function StyleCalendar() {
     const [date, setDate] = useState(new Date());
 
+    const [currentTime, setCurrentTime] = useState();
+    useEffect(() => {
+        let interval = setInterval( () => setCurrentTime(new Date().toLocaleTimeString()))
+    
+          return () => clearInterval(interval)
+        },[])
     return (
         <div className = "calendar">
             <div className='app-calendar'>
@@ -17,6 +23,7 @@ export default function StyleCalendar() {
                     <span className='bold'>Selected Date:</span>{' '}
                     {date.toDateString()}
                 </p>
+                <div id='time'>{currentTime}</div>
             </div>
         </div>
         
